5e5d1028ca89e43e9acfcba39336ab208d3ee28f
clement lion <clement.lionceau@gmail.com>
2026-03-12 18:46:51 +0100

    added exercise for linked list

---
+++ InterviewTraining/SortListOfLinkedLists.cs
@@ -0,0 +1,58 @@
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public static class SortListOfLinkedList
+{
+    public static ListNode MergeKLists(ListNode[] lists)
+    {
+        if (lists.Length == 0)
+        {
+            return null;
+        }
+
+        if (lists.Length == 1)
+        {
+            return lists[0];
+        }
+
+        int[] listVals = new int[lists.Length];
+        Array.Fill(listVals, int.MaxValue);
+        for (int i = 0; i < listVals.Length; i++)
+        {
+            if (lists[i] != null)
+            {
+                listVals[i] = lists[i].val;
+            }
+        }
+
+        int minVal = listVals.Min();
+        if (minVal == int.MaxValue)
+            return null;
+        int indexMin = listVals.IndexOf(minVal);
+        lists[indexMin] = lists[indexMin].next;
+        listVals[indexMin] = lists[indexMin] == null ? int.MaxValue : lists[indexMin].val;
+        ListNode beginning = new(minVal);
+        ListNode current = beginning;
+
+        while (true)
+        {
+            minVal = listVals.Min();
+            if (minVal == int.MaxValue)
+                return beginning;
+            indexMin = listVals.IndexOf(minVal);
+            lists[indexMin] = lists[indexMin].next;
+            listVals[indexMin] = lists[indexMin] == null ? int.MaxValue : lists[indexMin].val;
+            current.next = new(minVal);
+            current = current.next;
+        }
+    }
+}
