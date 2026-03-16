836b5758a980dedb2417a5f13d7c180d66c8b3d7
clement lion <clement.lionceau@gmail.com>
2026-03-16 11:51:34 +0100

    added optimal method to add temperatures

--- InterviewTraining/FindNumberOfDayBeforeGreaterTemperature.cs
+++ InterviewTraining/FindNumberOfDayBeforeGreaterTemperature.cs
@@ -31,4 +31,27 @@ public class TemperatureClass
         }
         return result;
     }
+
+    public static int[] DailyTemperaturesOpti(int[] temperatures)
+    {
+        var result = new int[temperatures.Length];
+        var stack = new Stack<int>();
+        for (int i = temperatures.Length - 1; i >= 0; i--)
+        {
+            while (stack.Count != 0 && temperatures[i] >= temperatures[stack.Peek()])
+            {
+                stack.Pop();
+            }
+            if (stack.Count == 0)
+            {
+                result[i] = 0;
+            }
+            else
+            {
+                result[i] = stack.Peek() - i;
+            }
+            stack.Push(i);
+        }
+        return result;
+    }
 }
